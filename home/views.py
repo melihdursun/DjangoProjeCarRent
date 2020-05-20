b55d878dbdbf6ea8bcf13c8aea24ab2830119bbd
melihdursun <melihdursunce@gmail.com>
2020-05-21 02:26:48 +0300

    ...

--- home/views.py
+++ home/views.py
@@ -1,11 +1,78 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
+from product.models import Product, Category, Images
 
 
 def index(request):
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    sliderdata = Product.objects.all()[:3]
+    category = Category.objects.all()
+    dayproducts = Product.objects.all()[:4]
+    lastproducts = Product.objects.all().order_by('-id')[:4]
+    randomproducts = Product.objects.all().order_by('?')[:4]
+    context = {'setting': setting,
+               'category': category,
+               'page': 'home',
+               'sliderdata': sliderdata,
+               'dayproducts': dayproducts,
+               'lastproducts': lastproducts,
+               'randomproducts': randomproducts,
+               }
     return render(request, 'index.html', context)
+
+
+def about(request):
+    setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
+    context = {'setting': setting,
+               'category': category,
+               'page': 'about'}
+    return render(request, 'about.html', context)
+
+def contact(request):
+
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"Mesajınız başarı ile gönderilmiştir. Teşekkür Ederiz")
+            return HttpResponseRedirect('/contact')
+
+    setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
+    form = ContactFormu()
+    context = {'setting': setting,
+               'category': category,
+               'form':form}
+    return render(request, 'contact.html', context)
+
+def category_products(request,id,slug):
+    category = Category.objects.all()
+    categorydata = Category.objects.get(pk=id)
+    products = Product.objects.filter(category_id=id)
+    context = {'products': products,
+               'category': category,
+               'categorydata': categorydata
+               }
+    return render(request, 'products.html', context)
+
+
+def product_detail(request,id,slug):
+    category = Category.objects.all()
+    product = Product.objects.get(pk=id)
+    images = Images.objects.filter(product_id=id)
+    context = {'product': product,
+               'category': category,
+               'images': images,
+               }
+    return render(request, 'product_detail.html', context)
